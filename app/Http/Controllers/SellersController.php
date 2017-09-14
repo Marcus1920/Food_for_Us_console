38810a7b2ac448c26e5338292eee88bc3239b8fb
Marcus <mark.mbayo.n@gmail.com>
2017-09-14 12:09:38 +0200

    update

--- app/Http/Controllers/SellersController.php
+++ app/Http/Controllers/SellersController.php
@@ -49,7 +49,8 @@ class SellersController extends Controller
 
             }
         }
-        return $nearSellers;
+        return json_encode(array($nearSellers));
+		// ->header('Content-Type', 'application/json');
     }
 
 
