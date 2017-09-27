804351af4e9c4b48a12d79599e2280f38b90b7ff
Hyfos2 <bthighforce@gmail.com>
2017-09-27 16:18:50 +0200

    done

--- app/Http/Controllers/SellersController.php
+++ app/Http/Controllers/SellersController.php
@@ -254,11 +254,12 @@ class SellersController extends Controller
                                     ->where('api_key',Input::get('apiKey'))
                                     ->first();
 
-        $sellerDetails           = Sellers_details_tabs::select('id')
+        $sellerDetails          = Sellers_details_tabs::select('id')
                                                 ->where('id',$newUserDetails->id)
                                                 ->first();
 
-        $changeDefaultLocation = UserDefaultLocation::where('userId',$newUserDetails->id)
+        $changeDefaultLocation  = UserDefaultLocation::where('userId',$newUserDetails->id)
+
                 ->update([
                     'gps_lat'  =>Input::get('gps_lat'),
                     'gps_long' =>Input::get('gps_long'),
