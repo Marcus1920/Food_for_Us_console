0cea652749dcbcd81985a6d4f766734ad4ecee30
ZiphoDanca <21335890@dut4life.ac.za>
2018-03-13 16:25:01 +0200

    updates

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -15,10 +15,26 @@ class NotificationsController extends Controller
 
         $user  = NewUser::where('api_key',$api_key)->first();
 
-        $notifications = Notification::select('id','new_user_id','PostId','ProductName','Message','Status','created_at')
-            ->where('new_user_id',$user->id)
-            ->where('Status',0)
-            ->orderBy('id','ASC')-> get();
+        $notifications=\DB::table('notifications')
+            ->join('sellers_details_tabs', 'notifications.PostId', '=', 'sellers_details_tabs.id')
+            ->where('notifications.new_user_id',$user->id)
+            ->where('notifications.Status',0)
+            ->select(
+                \DB::raw(
+                    "
+                        notifications.id,
+                        notifications.new_user_id,
+                        notifications.PostId,
+                        sellers_details_tabs.productPicture,
+                        notifications.ProductName,
+                        notifications.Message,
+                        notifications.Status,
+                        notifications.created_at
+                    "
+                )
+            )
+            ->orderBy('id','ASC')
+            ->get();
 
         return response()->json($notifications);
     }
